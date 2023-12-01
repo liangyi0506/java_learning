05efc0b8c1825a626414213393cb4ebdcd2d8823
liangyi0506 <liangyi1999@yeah.net>
2023-12-01 22:35:56 +0800

    更新

--- springmvc/springmvc-base-output-03/src/main/java/com/liangyi/config/MvcConfig.java
+++ springmvc/springmvc-base-output-03/src/main/java/com/liangyi/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.liangyi.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
     // 视图解析器ViewResolver
-
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         // 规定寻找jsp文件的前后缀
@@ -19,5 +19,13 @@ public class MvcConfig implements WebMvcConfigurer {
         // 后缀：.jsp
         // 总之就是通过前面的路径和后面的后缀，在controller层能找到即可
         registry.jsp("/WEB-INF/view", ".jsp");
+    }
+
+
+    // 开启静态资源访问
+    // 直接输入webapp下的静态资源即可，例如访问redpig.jpg 只需要输入/image/redpig.jpg
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
