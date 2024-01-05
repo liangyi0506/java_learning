f4a29d5dbcb93b1e055cbcb413d20b184a3494d2
liangyi <liangyi1999@yeah.net>
2024-01-05 23:32:57 +0800

    springboot整合springmvc

---
+++ springboot/springboot-base-springmvc-03/src/main/java/com/liangyi/interceptor/MyInterceptor.java
@@ -0,0 +1,13 @@
+package com.liangyi.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("request = " +request + "\n response = " + response + "\n handler = " + handler);
+        return true;
+    }
+}
